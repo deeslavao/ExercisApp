9ccef1ad677a429fa69cbf11e7af4345b4e7b4f5
Andres <abetancurthb@unal.edu.co>
2021-11-17 16:10:30 -0500

    conceptProofLogin

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,22 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <router-link to="/"></router-link>
   <router-view/>
 </template>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #161032;
 }
 
 #nav {
   padding: 30px;
+
 }
 
 #nav a {
---
+++ front/src/components/LoginBox.vue
@@ -0,0 +1,61 @@
+<template>
+  <form id ="LoginForm">
+    <input type="email">
+    <input type="password">
+    <div id = "LoginButtons">
+      <button id="aceptar">
+        Iniciar Secion
+      </button>
+      <button id="registrar">
+        Registrarse
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Road+Rage&display=swap');
+button {
+  display:block;
+  margin: 1rem auto;
+  padding: 0.7rem 3rem;
+  border-radius: 0.3rem;
+  border: 0;
+  color: #FFFBF4;
+  font-family: 'Josefin Sans', sans-serif;
+}
+
+#LoginForm {
+    box-shadow: 0 0.65rem 1rem rgba(0,0,0,1);
+    background-color: #FFFBF4;
+    border-radius: 0.5rem;
+    padding: 1rem 0rem;
+    display: block;
+    width: 15rem;
+    margin-left: 60rem;
+    margin-top: -23rem;
+}
+
+#LoginForm input {
+  height: 1.5rem;
+  background-color: #DBDBDB;
+  margin: 1rem 0;
+  border-radius: 0.2rem;
+  border-color: #DBDBDB;
+  border: 0;
+}
+
+#LoginButtons #aceptar {
+  background-color: #08C0FF;
+}
+
+#LoginButtons #registrar {
+  background-color: #D71400;
+}
+</style>

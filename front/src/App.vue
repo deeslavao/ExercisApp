32628e5eb6feae3ae8d30a80fa14523e7ece5a94
MIGUEL CURREA <mcurream@unal.edu.co>
2021-11-29 11:15:46 -0500

    New Home Page

---
+++ front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <md-toolbar>
+      <h3 class="md-title" style="flex: 1"><a href="default.aspx"><img src=".\assets\Appxercise-01.png"></a></h3>
+      <md-button style="color:#ee2d2b">Registro <i class="material-icons" style="vertical-align:middle">dashboard</i></md-button>
+      <md-button style="color:#ee2d2b">Ingresar <i class="material-icons" style="vertical-align:middle">login</i></md-button>
+    </md-toolbar>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+   background-color: #ee2d2b
+  }
+.container {
+    display: flex;
+    margin: 20px;
+    background-color: #ee2d2b
+}
+</style>
